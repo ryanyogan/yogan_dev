732822858135f510d35b075e83f57c43d67ec1a1
ryan <ryanyogan@gmail.com>
2021-07-08 10:39:06 -0500

    added article and content structures, added tesla & hackney deps

---
+++ lib/yogan_dev/article.ex
@@ -0,0 +1,29 @@
+defmodule YoganDev.Article do
+  @moduledoc """
+  Represents an individual `Article` (Blog Post)
+  """
+
+  alias __MODULE__
+
+  @type t :: %Article{
+          id: String.t(),
+          slug: String.t(),
+          title: String.t(),
+          description: String.t(),
+          image: String.t(),
+          content: String.t(),
+          author: String.t(),
+          published_at: Date.t()
+        }
+
+  defstruct [
+    :id,
+    :slug,
+    :title,
+    :description,
+    :image,
+    :content,
+    :author,
+    :published_at
+  ]
+end
---
+++ lib/yogan_dev/content.ex
@@ -0,0 +1,27 @@
+defmodule YoganDev.Content do
+  @moduledoc """
+  Represents each sections content
+  """
+
+  alias __MODULE__
+
+  @type t :: %Content{
+          id: String.t(),
+          position: non_neg_integer(),
+          type: String.t(),
+          title: String.t(),
+          content: String.t(),
+          image: String.t(),
+          styles: String.t()
+        }
+
+  defstruct [
+    :id,
+    :position,
+    :type,
+    :title,
+    :content,
+    :image,
+    :styles
+  ]
+end
